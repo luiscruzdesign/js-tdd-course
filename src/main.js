9ab9b655e0a8594b4233ebcd1334ae2b4b8cd03a
Luis Eduardo setin de carvalho cruz <icruz@Mac-mini-de-Administrador.local>
2019-05-31 15:06:32 -0300

    Add eslint

---
+++ src/main.js
@@ -0,0 +1,10 @@
+const a = 3;
+const b = 2;
+
+const c = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt tincidunt nisi eu venenatis. Sed cursus metus.';
+
+function sum() {
+  return a + b + c;
+}
+
+sum();
