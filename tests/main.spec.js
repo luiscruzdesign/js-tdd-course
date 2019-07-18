6bde8460973bc28a5a56a9962a4611695c48d013
Luis Eduardo setin de carvalho cruz <luiscruzdesign@gmail.com>
2019-07-18 11:19:21 -0300

    Exemplo de erros no mocha

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -9,11 +9,17 @@ describe('Main', function () {
         // espera que aconteça
         // Entrada de dados / método sum(2,2)
         // Espera retornar (4) => true | (3) => false => broken test
+        throw new Error('just an error');
       });
     });
 
     context('Case 2', function () {
-
+      it('should happen mimimi', function () {
+        // espera que aconteça
+        // Entrada de dados / método sum(2,2)
+        // Espera retornar (4) => true | (3) => false => broken test
+        throw new Error('just an error');
+      });
     });
   });
 
