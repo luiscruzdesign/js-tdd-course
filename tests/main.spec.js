3080b988370b2483710af932fd7e9f2b23da2b99
Luis Eduardo setin de carvalho cruz <luiscruzdesign@gmail.com>
2019-07-19 11:09:06 -0300

    Testes before, after, beforeEach, afterEach

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -3,31 +3,40 @@
 /* global describe,it */
 /* eslint-env mocha */
 describe('Main', function () {
+  let arr;
+
   // roda uma vez antes do bloco
   before(function () {
-    console.log('before');
+    // inicia conexão no banco
+    // criar conjunto de dados
   });
 
   // roda uma vez depois do bloco
   after(function () {
-    console.log('after');
+    // termina conexão no banco
+    // apagar conjunto de dados
   });
 
   // roda todas as vezes, antes de cada bloco
   beforeEach(function () {
-    console.log('beforeEach');
+    arr = [1, 2, 3];
   });
 
   // roda todas as vezes, depois de cada bloco
   afterEach(function () {
-    console.log('afterEach');
   });
 
-  it('test 1', function () {
-    console.log('test 1');
+  it('should have a size o 4 when push another value to the array', function () {
+    arr.push(4);
+    console.log(arr.length); // 4
   });
 
-  it('test 2', function () {
-    console.log('test 2');
+  it('should have a size o 2 when pop a value to the array', function () {
+    arr.pop();
+    console.log(arr.length); // 2
+  });
+
+  it('should remove 3 when use pop in the array', function () {
+    console.log(arr.pop() === 3); // true
   });
 });
