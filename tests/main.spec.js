2b7d69e6f2f66e29e69cf453d3d677978a5c8dca
Luis Eduardo setin de carvalho cruz <luiscruzdesign@gmail.com>
2019-07-22 17:37:13 -0300

    Métodos do Chai

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,42 +1,40 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable prefer-destructuring */
 /* eslint-disable func-names */
 /* eslint-disable prefer-arrow-callback */
 /* global describe,it */
 /* eslint-env mocha */
+const expect = require('chai').expect;
+
 describe('Main', function () {
   let arr;
-
-  // roda uma vez antes do bloco
-  before(function () {
-    // inicia conexão no banco
-    // criar conjunto de dados
-  });
-
-  // roda uma vez depois do bloco
-  after(function () {
-    // termina conexão no banco
-    // apagar conjunto de dados
-  });
 
   // roda todas as vezes, antes de cada bloco
   beforeEach(function () {
     arr = [1, 2, 3];
   });
 
-  // roda todas as vezes, depois de cada bloco
-  afterEach(function () {
+  // testar tipos ou se existe (smoke test)
+  it('should be an array', function () {
+    expect(arr).to.be.a('array');
   });
 
   it('should have a size o 4 when push another value to the array', function () {
     arr.push(4);
-    console.log(arr.length); // 4
+    expect(arr).to.have.lengthOf(4);
+  });
+
+  it('should remove 3 when use pop in the array', function () {
+    arr.pop();
+    expect(arr).to.not.include(3);
+  });
+
+  it('should return 3 qhen pop 3 from the array', function () {
+    expect(arr.pop() === 3).to.be.true;
   });
 
   it('should have a size o 2 when pop a value to the array', function () {
     arr.pop();
-    console.log(arr.length); // 2
-  });
-
-  it('should remove 3 when use pop in the array', function () {
-    console.log(arr.pop() === 3); // true
+    expect(arr).to.have.lengthOf(2);
   });
 });
