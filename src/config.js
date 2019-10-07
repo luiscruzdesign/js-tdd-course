62463da59175c22179808286da89bdf82c1a9064
Luis Eduardo setin de carvalho cruz <icruz@Mac-mini-de-Administrador.local>
2019-10-07 18:58:58 -0300

    Add authorization flow

--- src/config.js
+++ src/config.js
@@ -1,2 +1,10 @@
 // eslint-disable-next-line import/prefer-default-export
+const TOKEN_API = 'BQDNtbt3Ykxp2Z54iG06G8p5yvFQDZ74-li37thAInAGSLaIF6-o3ZiC0cOZhs41qO8MPGhqtxH8ng-be8WvEN9XsYhN0JlnQsbgSUJTGuyMqwKt4Ver_L9QGGFWbvcVRTSNPtcg6QQt2xxVcy54g-0';
+
 export const API_URL = 'https://api.spotify.com/v1';
+
+export const HEADERS = {
+  headers: {
+    Authorization: `Bearer ${TOKEN_API}`,
+  },
+};
